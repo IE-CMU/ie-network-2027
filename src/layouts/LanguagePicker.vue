8687f1f53911406c1f526d4088cb2ffa830cdf44
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2026-05-15 19:38:08 +0700

    feat: implement LanguagePicker component and update Menu layout for improved localization

---
+++ src/layouts/LanguagePicker.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { navigate } from 'astro:transitions/client'
+
+interface Props {
+  locale: 'th' | 'en'
+  items: Record<
+    string,
+    {
+      en: string
+      th: string
+      url: string
+    }
+  >
+}
+const { locale, items } = defineProps<Props>()
+
+function handleLocaleChange(event: Event) {
+  const selectElement = event.target as HTMLSelectElement
+  const selectedLocale = selectElement.value
+  console.log('Selected locale:', selectedLocale)
+  console.log('Items:', items)
+  //   const nextUrl = items[selectedLocale].url
+  // navigate(nextUrl)
+}
+</script>
+
+<template>
+  <select @change="handleLocaleChange" class="">
+    <option value="th" :selected="locale === 'th'">ไทย</option>
+    <option value="en" :selected="locale === 'en'">English</option>
+  </select>
+</template>
